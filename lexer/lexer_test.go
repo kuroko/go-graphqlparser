977e6ec378485e848f8b328f8beb7e0dbe1a1d59
Elliot Wright <wright.elliot@gmail.com>
2018-04-02 17:12:03 +0100

    Updated tests to use subtests.

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -44,195 +44,194 @@ func BenchmarkLexer_ScanExt(b *testing.B) {
 	}
 }
 
-func TestLexerScanNumber(t *testing.T) {
-	tests := []struct {
-		name      string
-		input     string
-		wantToken Token
-		wantErr   bool
-	}{
-		// Happy inputs.
-		{
-			name:  "lone zero is valid",
-			input: "0",
-			wantToken: Token{
-				Type:    token.IntValue,
-				Literal: "0",
+func TestLexer_Scan(t *testing.T) {
+	t.Run("scanNumber()", func(t *testing.T) {
+		tests := []struct {
+			name      string
+			input     string
+			wantToken Token
+			wantErr   bool
+		}{
+			// Happy inputs.
+			{
+				name:  "lone zero is valid",
+				input: "0",
+				wantToken: Token{
+					Type:    token.IntValue,
+					Literal: "0",
+				},
+				wantErr: false,
 			},
-			wantErr: false,
-		},
-		{
-			name:  "positive int is valid",
-			input: "123456789",
-			wantToken: Token{
-				Type:    token.IntValue,
-				Literal: "123456789",
+			{
+				name:  "positive int is valid",
+				input: "123456789",
+				wantToken: Token{
+					Type:    token.IntValue,
+					Literal: "123456789",
+				},
+				wantErr: false,
 			},
-			wantErr: false,
-		},
-		{
-			name:  "negative int is valid",
-			input: "-123456789",
-			wantToken: Token{
-				Type:    token.IntValue,
-				Literal: "-123456789",
+			{
+				name:  "negative int is valid",
+				input: "-123456789",
+				wantToken: Token{
+					Type:    token.IntValue,
+					Literal: "-123456789",
+				},
+				wantErr: false,
 			},
-			wantErr: false,
-		},
-		{
-			name:  "positive float is valid",
-			input: "1.1",
-			wantToken: Token{
-				Type:    token.FloatValue,
-				Literal: "1.1",
+			{
+				name:  "positive float is valid",
+				input: "1.1",
+				wantToken: Token{
+					Type:    token.FloatValue,
+					Literal: "1.1",
+				},
+				wantErr: false,
 			},
-			wantErr: false,
-		},
-		{
-			name:  "negative float is valid",
-			input: "-1.1",
-			wantToken: Token{
-				Type:    token.FloatValue,
-				Literal: "-1.1",
+			{
+				name:  "negative float is valid",
+				input: "-1.1",
+				wantToken: Token{
+					Type:    token.FloatValue,
+					Literal: "-1.1",
+				},
+				wantErr: false,
 			},
-			wantErr: false,
-		},
-		{
-			name:  "exponent is valid",
-			input: "10E99",
-			wantToken: Token{
-				Type:    token.FloatValue,
-				Literal: "10E99",
+			{
+				name:  "exponent is valid",
+				input: "10E99",
+				wantToken: Token{
+					Type:    token.FloatValue,
+					Literal: "10E99",
+				},
+				wantErr: false,
 			},
-			wantErr: false,
-		},
-		{
-			name:  "negative exponent is valid",
-			input: "-10E99",
-			wantToken: Token{
-				Type:    token.FloatValue,
-				Literal: "-10E99",
+			{
+				name:  "negative exponent is valid",
+				input: "-10E99",
+				wantToken: Token{
+					Type:    token.FloatValue,
+					Literal: "-10E99",
+				},
+				wantErr: false,
 			},
-			wantErr: false,
-		},
-		{
-			name:  "float, positive exponent is valid",
-			input: "1.1e99",
-			wantToken: Token{
-				Type:    token.FloatValue,
-				Literal: "1.1e99",
+			{
+				name:  "float, positive exponent is valid",
+				input: "1.1e99",
+				wantToken: Token{
+					Type:    token.FloatValue,
+					Literal: "1.1e99",
+				},
+				wantErr: false,
 			},
-			wantErr: false,
-		},
-		{
-			name:  "float, negative exponent is valid",
-			input: "-1.1e-99",
-			wantToken: Token{
-				Type:    token.FloatValue,
-				Literal: "-1.1e-99",
+			{
+				name:  "float, negative exponent is valid",
+				input: "-1.1e-99",
+				wantToken: Token{
+					Type:    token.FloatValue,
+					Literal: "-1.1e-99",
+				},
+				wantErr: false,
 			},
-			wantErr: false,
-		},
-		// Errorful inputs.
-		{
-			name:  "negative symbol with no following digits is invalid",
-			input: "-",
-			wantToken: Token{
-				Type: token.Illegal,
+			// Errorful inputs.
+			{
+				name:  "negative symbol with no following digits is invalid",
+				input: "-",
+				wantToken: Token{
+					Type: token.Illegal,
+				},
+				wantErr: true,
 			},
-			wantErr: true,
-		},
-		{
-			name:  "negative symbol with non digit after is invalid",
-			input: "-x",
-			wantToken: Token{
-				Type: token.Illegal,
+			{
+				name:  "negative symbol with non digit after is invalid",
+				input: "-x",
+				wantToken: Token{
+					Type: token.Illegal,
+				},
+				wantErr: true,
 			},
-			wantErr: true,
-		},
-		{
-			name:  "non digit after decimal point is invalid",
-			input: "1.x",
-			wantToken: Token{
-				Type: token.Illegal,
+			{
+				name:  "non digit after decimal point is invalid",
+				input: "1.x",
+				wantToken: Token{
+					Type: token.Illegal,
+				},
+				wantErr: true,
 			},
-			wantErr: true,
-		},
-		{
-			name:  "zero followed by number is invalid",
-			input: "01",
-			wantToken: Token{
-				Type: token.Illegal,
+			{
+				name:  "zero followed by number is invalid",
+				input: "01",
+				wantToken: Token{
+					Type: token.Illegal,
+				},
+				wantErr: true,
 			},
-			wantErr: true,
-		},
-	}
+		}
 
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			bs := []byte(tt.input)
-			l := New(bs)
-			gotT, err := l.Scan()
-			if !tt.wantErr && err != nil {
-				t.Errorf("Lexer.scanNumber() error = %v, wantErr %v", err, tt.wantErr)
-				return
-			}
+		for _, tt := range tests {
+			t.Run(tt.name, func(t *testing.T) {
+				bs := []byte(tt.input)
+				l := New(bs)
+				gotT, err := l.Scan()
+				if !tt.wantErr && err != nil {
+					t.Errorf("Lexer.scanNumber() error = %v, wantErr %v", err, tt.wantErr)
+					return
+				}
 
-			isTypeMatch := gotT.Type == tt.wantToken.Type
-			isLiteralMatch := gotT.Literal == tt.wantToken.Literal
-			if !isTypeMatch || !isLiteralMatch {
-				t.Errorf("Lexer.scanNumber() = %+v, want %+v", gotT, tt.wantToken)
-			}
-		})
-	}
-}
+				isTypeMatch := gotT.Type == tt.wantToken.Type
+				isLiteralMatch := gotT.Literal == tt.wantToken.Literal
+				if !isTypeMatch || !isLiteralMatch {
+					t.Errorf("Lexer.scanNumber() = %+v, want %+v", gotT, tt.wantToken)
+				}
+			})
+		}
+	})
 
-func TestLexerScanComment(t *testing.T) {
-	tests := []struct {
-		name      string
-		input     string
-		wantToken Token
-		wantErr   bool
-	}{
-		// {
-		// 	name: "single line comment valid",
-		// 	input: `# comment
-		// 	query
-		// 	`,
-		// 	wantToken: Token{
-		// 		Type:    token.Name,
-		// 		Literal: "query",
-		// 	},
-		// 	wantErr: false,
-		// },
-		{
-			name:  "single line comment valid",
-			input: "# comment" + string(lf) + "foo",
-			wantToken: Token{
-				Type:     token.Name,
-				Literal:  "foo",
-				Position: 4,
-				Line:     2,
+	t.Run("scanName()", func(t *testing.T) {
+
+	})
+
+	t.Run("scanPunctuator()", func(t *testing.T) {
+
+	})
+
+	t.Run("scanComment", func(t *testing.T) {
+		tests := []struct {
+			name      string
+			input     string
+			wantToken Token
+			wantErr   bool
+		}{
+			{
+				name:  "single line comment valid",
+				input: "# comment" + string(lf) + "foo",
+				wantToken: Token{
+					Type:     token.Name,
+					Literal:  "foo",
+					Position: 4,
+					Line:     2,
+				},
+				wantErr: false,
 			},
-			wantErr: false,
-		},
-	}
+		}
 
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			bs := []byte(tt.input)
-			l := New(bs)
-			gotT, err := l.Scan()
-			if !tt.wantErr && err != nil {
-				t.Errorf("Lexer.scanComment() error = %v, wantErr %v", err, tt.wantErr)
-				return
-			}
+		for _, tt := range tests {
+			t.Run(tt.name, func(t *testing.T) {
+				bs := []byte(tt.input)
+				l := New(bs)
+				gotT, err := l.Scan()
+				if !tt.wantErr && err != nil {
+					t.Errorf("Lexer.scanComment() error = %v, wantErr %v", err, tt.wantErr)
+					return
+				}
 
-			isTypeMatch := gotT.Type == tt.wantToken.Type
-			isLiteralMatch := gotT.Literal == tt.wantToken.Literal
-			if !isTypeMatch || !isLiteralMatch {
-				t.Errorf("Lexer.scanComment() = %+v, want %+v", gotT, tt.wantToken)
-			}
-		})
-	}
+				isTypeMatch := gotT.Type == tt.wantToken.Type
+				isLiteralMatch := gotT.Literal == tt.wantToken.Literal
+				if !isTypeMatch || !isLiteralMatch {
+					t.Errorf("Lexer.scanComment() = %+v, want %+v", gotT, tt.wantToken)
+				}
+			})
+		}
+	})
 }
