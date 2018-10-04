d316bea6326ce1156fc2858d45c0dbbe8381f824
Luke Vear <vearluke@googlemail.com>
2018-10-04 15:30:14 +0100

    validator: used test schema from spec

--- validator/validator_test.go
+++ validator/validator_test.go
@@ -16,18 +16,56 @@ import (
 var update = flag.Bool("update", false, "update golden record files?")
 
 var schema = strings.TrimSpace(`
-schema {
-  query: MyQueryRootType
-  mutation: MyMutationRootType
+type Query {
+  dog: Dog
 }
 
-type MyQueryRootType {
-  someField: String
+enum DogCommand { 
+  SIT
+  DOWN
+  HEEL
 }
 
-type MyMutationRootType {
-  setSomeField(to: String): String
+type Dog implements Pet {
+  name: String!
+  nickname: String
+  barkVolume: Int
+  doesKnowCommand(dogCommand: DogCommand!): Boolean!
+  isHousetrained(atOtherHomes: Boolean): Boolean!
+  owner: Human
 }
+
+interface Sentient {
+  name: String!
+}
+
+interface Pet {
+  name: String!
+}
+
+type Alien implements Sentient {
+  name: String!
+  homePlanet: String
+}
+
+type Human implements Sentient {
+  name: String!
+}
+
+enum CatCommand {
+  JUMP
+}
+
+type Cat implements Pet {
+  name: String!
+  nickname: String
+  doesKnowCommand(catCommand: CatCommand!): Boolean!
+  meowVolume: Int
+}
+
+union CatOrDog = Cat | Dog
+union DogOrHuman = Dog | Human
+union HumanOrAlien = Human | Alien
 `)
 
 func TestValidator_applyRulesGolden(t *testing.T) {
