f5f8f56b330d9aacfe98b87c1b64e61a6aa99457
Luke Vear <vearluke@googlemail.com>
2018-10-08 13:07:31 +0100

    validator: more experimentation

--- ast/validate.go
+++ ast/validate.go
@@ -1,14 +1,21 @@
 package ast
 
+import "fmt"
+
 type ValidationContext struct {
 	Schema Document
 }
 
-func (d *Document) validate(vctx *ValidationContext) *Errors {
+type Vdata struct {
+	operationNames     map[string]bool
+	anonOperationCount int
+}
+
+func (d *Document) validate(vctx *ValidationContext, vd *Vdata) *Errors {
 	var errs *Errors
 
 	d.Definitions.ForEach(func(definition Definition, _ int) {
-		e := definition.validate(vctx)
+		e := definition.validate(vctx, vd)
 		if e == nil {
 			return
 		}
@@ -17,32 +24,54 @@ func (d *Document) validate(vctx *ValidationContext) *Errors {
 		errs = e
 	})
 
+	if vd.anonOperationCount > 1 {
+		err := fmt.Errorf("seen %d shorthand queries", vd.anonOperationCount)
+		errs = errs.Add(err)
+	}
+
 	return errs.Reverse()
 }
 
-func (d *Definition) validate(vctx *ValidationContext) *Errors {
+func (d *Definition) validate(vctx *ValidationContext, vd *Vdata) *Errors {
 	var errs *Errors
 
 	switch d.Kind {
 	case DefinitionKindExecutable:
-		errs = d.ExecutableDefinition.validate(vctx)
+		errs = d.ExecutableDefinition.validate(vctx, vd)
+
 	case DefinitionKindTypeSystem:
-		errs = d.TypeSystemDefinition.validate(vctx)
+		err := fmt.Errorf("definition is of type TypeSystemDefinition, must be executable")
+		errs = errs.Add(err)
+
+		errs = d.TypeSystemDefinition.validate(vctx, vd)
+
 	case DefinitionKindTypeSystemExtension:
-		errs = d.TypeSystemExtension.validate(vctx)
+		err := fmt.Errorf("definition is of type TypeSystemExtension, must be executable")
+		errs = errs.Add(err)
+
+		errs = d.TypeSystemExtension.validate(vctx, vd)
 	}
 
 	return errs
 }
 
-func (d *ExecutableDefinition) validate(vctx *ValidationContext) *Errors {
+func (d *ExecutableDefinition) validate(vctx *ValidationContext, vd *Vdata) *Errors {
 	var errs *Errors
+
+	if vd.operationNames[d.Name] {
+		err := fmt.Errorf("operation definitions should be unique, seen %s more than once", d.Name)
+		errs = errs.Add(err)
+	}
+	vd.operationNames[d.Name] = true
 
 	switch d.Kind {
 	case ExecutableDefinitionKindOperation:
 
 		switch d.OperationType {
 		case OperationTypeQuery:
+			if d.ShorthandQuery {
+				vd.anonOperationCount++
+			}
 		case OperationTypeMutation:
 		case OperationTypeSubscription:
 		}
@@ -54,12 +83,13 @@ func (d *ExecutableDefinition) validate(vctx *ValidationContext) *Errors {
 	return errs
 }
 
-func (d *TypeSystemDefinition) validate(vctx *ValidationContext) *Errors {
+func (d *TypeSystemDefinition) validate(vctx *ValidationContext, vd *Vdata) *Errors {
 	var errs *Errors
+
 	return errs
 }
 
-func (d *TypeSystemExtension) validate(vctx *ValidationContext) *Errors {
+func (d *TypeSystemExtension) validate(vctx *ValidationContext, vd *Vdata) *Errors {
 	var errs *Errors
 	return errs
 }
