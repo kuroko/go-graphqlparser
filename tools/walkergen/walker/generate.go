9a40c734043bd645cf0ac9c7f709c603a7c068fe
Elliot Wright <wright.elliot@gmail.com>
2018-11-18 19:51:05 +0000

    Added some more comments. Removed a TODO.

--- tools/walkergen/walker/generate.go
+++ tools/walkergen/walker/generate.go
@@ -8,7 +8,6 @@ import (
 )
 
 // TODO: check output.
-// TODO: clean code.
 func Generate(w io.Writer, s *goast.Symbols) {
 	var typeNames []string
 	for tn := range s.AST.Structs {
@@ -58,20 +57,26 @@ func Generate(w io.Writer, s *goast.Symbols) {
 	}
 }
 
+// isFieldPointer checks the symbol table for references of the given type name on fields of other
+// types, returning true if the given type name is ever used as a pointer.
 func isFieldPointer(s *goast.Symbols, tn string) bool {
 	for _, strc := range s.AST.Structs {
 		if fld, ok := strc.Fields[tn]; ok {
 			return fld.IsPointer
 		}
 	}
+
 	return false
 }
 
+// isFieldArray checks the symbol table for references of the given type name on fields of other
+// types, returning true if the given type name is ever used as an array.
 func isFieldArray(s *goast.Symbols, tn string) bool {
 	for _, strc := range s.AST.Structs {
 		if fld, ok := strc.Fields[tn]; ok {
 			return fld.IsArray
 		}
 	}
+
 	return false
 }
