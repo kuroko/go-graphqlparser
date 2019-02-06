ee4e37376392f7a03fa0b93b46a5f2395672602e
Luke Vear <vearluke@googlemail.com>
2019-02-06 18:24:57 +0000

    rules: frag composite type error

--- validation/rules/rule_fragments_on_composite_types.go
+++ validation/rules/rule_fragments_on_composite_types.go
@@ -1,8 +1,24 @@
 package rules
 
 import (
+	"fmt"
+
+	"github.com/bucketd/go-graphqlparser/graphql"
 	"github.com/bucketd/go-graphqlparser/validation"
 )
 
 // fragmentsOnCompositeTypes ...
 func fragmentsOnCompositeTypes(w *validation.Walker) {}
+
+func fragmentsOnCompositeTypesError(fragName, typeName string, line, col int) graphql.Error {
+	message := "Fragment "
+	if len(fragName) != 0 {
+		message += fragName + " "
+	}
+	message += fmt.Sprintf(`cannot condition on non composite type "%s"`, typeName)
+
+	return graphql.NewError(
+		message,
+		// TODO: Location.
+	)
+}
