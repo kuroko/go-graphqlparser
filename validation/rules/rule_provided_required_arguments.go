149de9fd03baf0ed55276057b0f04176dfce928f
Luke Vear <vearluke@googlemail.com>
2019-03-27 21:23:01 +0000

    rules: ProvidedRequiredArguments errors and tests

--- validation/rules/rule_provided_required_arguments.go
+++ validation/rules/rule_provided_required_arguments.go
@@ -1,8 +1,25 @@
 package rules
 
 import (
+	"github.com/bucketd/go-graphqlparser/graphql/types"
 	"github.com/bucketd/go-graphqlparser/validation"
 )
 
 // ProvidedRequiredArguments ...
 func ProvidedRequiredArguments(w *validation.Walker) {}
+
+// MissingFieldArgMessage ...
+func MissingFieldArgMessage(fieldName, argName, t string, line, col int) types.Error {
+	return types.NewError(
+		"Field \"" + fieldName + "\" argument \"" + argName + "\" of type \"" + t + "\" is required, but it was not provided",
+		// TODO: Location.
+	)
+}
+
+// MissingDirectiveArgMessage ...
+func MissingDirectiveArgMessage(directiveName, argName, t string, line, col int) types.Error {
+	return types.NewError(
+		"Directive \"" + directiveName + "\" argument \"" + argName + "\" of type \"" + t + "\" is required, but it was not provided",
+		// TODO: Location.
+	)
+}
