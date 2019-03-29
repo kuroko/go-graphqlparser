fc4613057de60e2edfa07580f6db7ac94a77c33b
Elliot Wright <wright.elliot@gmail.com>
2019-03-29 21:27:21 +0000

    validation: More benchmarks, validation only this time.

--- validation/validate_test.go
+++ validation/validate_test.go
@@ -3,6 +3,7 @@ package validation_test
 import (
 	"testing"
 
+	"github.com/bucketd/go-graphqlparser/graphql"
 	"github.com/bucketd/go-graphqlparser/graphql/types"
 	"github.com/bucketd/go-graphqlparser/language"
 	"github.com/bucketd/go-graphqlparser/validation"
@@ -10,7 +11,65 @@ import (
 	"github.com/davecgh/go-spew/spew"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	"github.com/vektah/gqlparser/ast"
+	"github.com/vektah/gqlparser/gqlerror"
+	"github.com/vektah/gqlparser/parser"
+	"github.com/vektah/gqlparser/validator"
+	_ "github.com/vektah/gqlparser/validator/rules"
 )
+
+func BenchmarkValidateSDL(b *testing.B) {
+	// Default query rules.
+	walker := validation.NewWalker(rules.SpecifiedSDL)
+
+	var errs *types.Errors
+
+	b.ReportAllocs()
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		b.StopTimer()
+
+		// When testing Vektah's parser, we can't re-use this, so let's do the same here, even
+		// though in our case we can re-use this document.
+		doc, err := graphql.Parse(schemaDoc)
+		require.NoError(b, err)
+
+		b.StartTimer()
+
+		errs = validation.ValidateSDL(doc, nil, walker)
+	}
+
+	b.StopTimer()
+
+	require.Nil(b, errs)
+}
+
+func BenchmarkVektahValidateSchema(b *testing.B) {
+	var gerr *gqlerror.Error
+
+	b.ReportAllocs()
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		b.StopTimer()
+
+		doc, err := parser.ParseSchemas(validator.Prelude, &ast.Source{
+			Name:  "test.graphqls",
+			Input: string(schemaDoc),
+		})
+
+		require.Nil(b, err)
+
+		b.StartTimer()
+
+		_, gerr = validator.ValidateSchemaDocument(doc)
+	}
+
+	b.StopTimer()
+
+	require.Nil(b, gerr)
+}
 
 func TestValidate(t *testing.T) {
 	// TODO: Move into golden test format.
