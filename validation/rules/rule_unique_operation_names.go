19cdd68f1a46350996e04c0fce903da39ca27003
Luke Vear <vearluke@googlemail.com>
2019-04-17 15:28:24 +0100

    rules: UniqueOperationNames tests
    added tests, error message and first pass of code
    wont work until we decorate context for non-sdl validation
    tests are ignored using // +build ignore

--- validation/rules/rule_unique_operation_names.go
+++ validation/rules/rule_unique_operation_names.go
@@ -1,8 +1,22 @@
 package rules
 
 import (
+	"github.com/bucketd/go-graphqlparser/ast"
+	"github.com/bucketd/go-graphqlparser/graphql/types"
 	"github.com/bucketd/go-graphqlparser/validation"
 )
 
-// UniqueOperationNames ...
-func UniqueOperationNames(w *validation.Walker) {}
+// DuplicateOperationNameError ...
+func DuplicateOperationNameError(operationName string, line, col int) types.Error {
+	return types.NewError("There can be only one operation named " + operationName + ".")
+}
+
+// UniqueOperationNames must each defined operation have.
+func UniqueOperationNames(w *validation.Walker) {
+	w.AddOperationDefinitionEnterEventHandler(func(ctx *validation.Context, od *ast.OperationDefinition) {
+		nameMatchDef, ok := ctx.OperationDefinitions[od.Name]
+		if ok && nameMatchDef != od {
+			ctx.AddError(DuplicateOperationNameError(od.Name, 0, 0))
+		}
+	})
+}
--- validation/rules/rule_unique_operation_names_test.go
+++ validation/rules/rule_unique_operation_names_test.go
@@ -1,13 +1,116 @@
+// +build ignore
+
 package rules_test
 
 import (
 	"testing"
 
+	"github.com/bucketd/go-graphqlparser/graphql/types"
 	"github.com/bucketd/go-graphqlparser/validation/rules"
 )
 
 func TestUniqueOperationNames(t *testing.T) {
-	tt := []ruleTestCase{}
+	tt := []ruleTestCase{
+		{
+			msg: "no operations",
+			query: `
+			fragment fragA on Type {
+			  field
+			}
+			`,
+		},
+		{
+			msg: "one anon operation",
+			query: `
+			{
+			  field
+			}
+			`,
+		},
+		{
+			msg: "one named operation",
+			query: `
+			query Foo {
+			  field
+			}
+			`,
+		},
+		{
+			msg: "multiple operations",
+			query: `
+			query Foo {
+			  field
+			}
+			query Bar {
+			  field
+			}
+			`,
+		},
+		{
+			msg: "multiple operations of different types",
+			query: `
+			query Foo {
+			  field
+			}
+			mutation Bar {
+			  field
+			}
+			subscription Baz {
+			  field
+			}
+			`,
+		},
+		{
+			msg: "fragment and operation named the same",
+			query: `
+			query Foo {
+			  ...Foo
+			}
+			fragment Foo on Type {
+			  field
+			}
+			`,
+		},
+		{
+			msg: "multiple operations of same name",
+			query: `
+			query Foo {
+			  fieldA
+			}
+			query Foo {
+			  fieldB
+			}
+			`,
+			errs: (*types.Errors)(nil).
+				Add(rules.DuplicateOperationNameError("Foo", 0, 0)),
+		},
+		{
+			msg: "multiple ops of same name of different types (mutation)",
+			query: `
+			query Foo {
+			  fieldA
+			}
+			mutation Foo {
+			  fieldB
+			}
+			`,
+			errs: (*types.Errors)(nil).
+				Add(rules.DuplicateOperationNameError("Foo", 0, 0)),
+		},
+		{
+			msg: "multiple ops of same name of different types (subscription)",
+			query: `
+			query Foo {
+			  fieldA
+			}
+			subscription Foo {
+			  fieldB
+			}
+			`,
+			errs: (*types.Errors)(nil).
+				Add(rules.DuplicateOperationNameError("Foo", 0, 0)),
+		},
+	}
 
 	queryRuleTester(t, tt, rules.UniqueOperationNames)
 }
